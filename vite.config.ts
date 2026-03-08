5352ce484a0261c3cd5730449fe218a0dfff64c5
Mesut Sancak <mesut.sancak@elock2.de>
2026-03-09 00:25:16 +0100

    FIX Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/HiBuy-Vue/',   // ← exakt der Repository-Name, Groß/Kleinschreibung beachten!
+  base: '/',   // ← Geändert!
   plugins: [vue()],
   resolve: {
     alias: {
